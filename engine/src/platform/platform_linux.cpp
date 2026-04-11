28865bcb6c30fca6b65674c9cbbbc9043d82f190
EthanWolf714 <Emwolf16@yahoo.com>
2026-04-11 10:56:40 -0500

    linux platform layer completed and tested

--- engine/src/platform/platform_linux.cpp
+++ engine/src/platform/platform_linux.cpp
@@ -1,5 +1,35 @@
 #include "platform.h"
 
+
+#if FPLATFORM_LINUX
+#include <xcb/xcb.h>
+#include <X11/keysym.h>
+#include <X11/XKBlib.h> //sudo dnf install libx11-devel
+#include <X11/Xlib.h>
+#include <X11/Xlib-xcb.h> //sudo dnf install libxkbcommon-x11-devel
+#include <sys/time.h>
+#include <core/logger.h>
+
+#if _POST_C_SOURCE >= 199309L
+#include <time.h> //nanosleep
+#else
+#include <unistd.h> //usleep
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct internal_state {
+    Display *display;
+    xcb_connection_t *connection; //server connectiong
+    xcb_window_t window; //ref to window 
+    xcb_screen_t *screen; //ref to screen
+    xcb_atom_t wm_protocols;
+    xcb_atom_t wm_delete_win;
+} interntal_state;
+
 FAPI b8 platform_startup(
     platform_state* plat_state,
     const char* application_name,
@@ -7,48 +37,251 @@ FAPI b8 platform_startup(
     i32 y,
     i32 width,
     i32 height){
+        ///create internal state
+        //allocate memory for state, create the internal state the size of the internal state?
+        plat_state->internal_state = malloc(sizeof(internal_state));
+        //cold cast, how we handle void pointer types
+        internal_state *state = (internal_state *)plat_state->internal_state;
+
+        //connect to X
+        state ->display = XOpenDisplay(NULL);
+
+        //turn off key repeated
+        //this call is global across the OS, 
+        //key repeats won't work until reboot
+        XAutoRepeatOff(state->display);
+
         
-    }
+        //can't get display from connection
+        //this is why this is done this way, display to get connection
+        state ->connection = XGetXCBConnection(state->display);
+
+        if(xcb_connection_has_error(state->connection)){
+            FFATAL("Failed to connect to X server via XCB.");
+            return FALSE;
+        }
+
+        //Get data from X server
+        const struct xcb_setup_t *setup = xcb_get_setup(state -> connection);
+
+        //use iterator to loop through screens
+        //have to make this call or else we have no screen
+        xcb_screen_iterator_t it = xcb_setup_roots_iterator(setup);
+        int screen_p = 0;
+        for(i32 s = screen_p; s > 0; s--){
+            xcb_screen_next(&it);
+        }
+
+        //after looping assign it 
+        state->screen = it.data;
+
+        //allocate a XID for window to be created
+        state ->window = xcb_generate_id(state->connection);
+
+        //need to register what types of events you need
+        //XCB_CW_BACK_PIXEL = filling then window bg with single colour
+        //XCB_CW_EVENT_MASK is required
+        u32 event_mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+
+        //listen for keyboard and mous buttons
+
+        u32 event_values = XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE |
+                            XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE |
+                            XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_POINTER_MOTION |
+                            XCB_EVENT_MASK_STRUCTURE_NOTIFY;
+
+        //Values to be sent over XCB (bg colour, events)
+
+        u32 value_list[] = {state->screen->black_pixel, event_values};
+
+        //create window
+
+        xcb_void_cookie_t cookie = xcb_create_window(
+            state->connection, 
+            XCB_COPY_FROM_PARENT, //depth
+            state->window,
+            state->screen->root,  //parent
+            x,                      //x
+            y,                      //y
+            width,                  //width
+            height,                 //height
+            0,                      //no border
+            XCB_WINDOW_CLASS_INPUT_OUTPUT, //class
+            state->screen->root_visual,
+            event_mask,
+            value_list);
+        
+        //change title
+        xcb_change_property(
+            state->connection,
+            XCB_PROP_MODE_REPLACE,
+            state->window,
+            XCB_ATOM_WM_NAME,
+            XCB_ATOM_STRING,
+            8, //data should be view 8 bits at a time
+            strlen(application_name),
+            application_name);
+
+        //tell server to notify when wintdow manager
+        //attempts to destroy window
+        xcb_intern_atom_cookie_t wm_delete_cookie = xcb_intern_atom(
+            state->connection,
+            0,
+            strlen("WM_DELETE_WINDOW"),
+            "WM_DELETE_WINDOW");
+        xcb_intern_atom_cookie_t wm_protocols_cookie = xcb_intern_atom(
+            state->connection,
+            0,
+            strlen("WM_PROTOCOLS"),
+            "WM_PROTOCOLS");
+        xcb_intern_atom_reply_t *wm_delete_reply = xcb_intern_atom_reply(
+            state->connection,
+            wm_delete_cookie,
+            NULL);
+        
+        xcb_intern_atom_reply_t *wm_protocols_reply = xcb_intern_atom_reply(
+            state->connection,
+            wm_protocols_cookie,
+            NULL);
+        
+        state->wm_delete_win = wm_delete_reply->atom;
+        state->wm_protocols = wm_protocols_reply->atom;
+
+        xcb_change_property(
+            state->connection,
+            XCB_PROP_MODE_REPLACE,
+            state->window,
+            wm_protocols_reply->atom,
+            4,
+            32,
+            1,
+            &wm_delete_reply->atom);
+
+        //map window to screen
+        xcb_map_window(state->connection, state->window);
+
+        i32 stream_result = xcb_flush(state->connection);
+        if(stream_result <= 0){
+            FFATAL("An error occured when flushing the stream: %d", stream_result);
+            return FALSE;
+        }
+
+        return true;
+
+}
 
 
 FAPI void platform_shutdown(platform_state* plat_state){
+    //cold cast
+    internal_state *state = (internal_state *)plat_state->internal_state;
+    //turn on repeats since its global
+    XAutoRepeatOn(state->display);
+
+    xcb_destroy_window(state->connection, state->window);
 
 }
 
 FAPI b8 platform_pump_message(platform_state* plat_state){
+    internal_state *state = (internal_state *)plat_state->internal_state;
 
+    xcb_generic_event_t *event;
+    xcb_client_message_event_t *cm;
+
+    b8 quit_flagged = FALSE;
+
+
+    //poll for the event
+    while(event != 0){
+        event = xcb_poll_for_event(state->connection);
+        if(event == 0){
+            break;
+        }
+
+        switch (event->response_type & -0x80){
+            case XCB_KEY_PRESS:
+            case XCB_KEY_RELEASE: {
+                //key press and release
+            }break; 
+            case XCB_BUTTON_PRESS:
+            case XCB_BUTTON_RELEASE: {
+                // mouse button pressed and release
+            }
+
+            case XCB_MOTION_NOTIFY:
+                //mouse movement
+            break;
+
+            case XCB_CONFIGURE_NOTIFY:{
+                //resizing
+            }
+
+            case XCB_CLIENT_MESSAGE: {
+                cm = (xcb_client_message_event_t *) event;
+
+                //window close
+                if(cm->data.data32[0] == state->wm_delete_win){
+                    quit_flagged = TRUE;
+                }
+            }break;
+            default:
+                //somthing
+                break;
+        }
+
+        free(event);
+    }
+
+    return !quit_flagged;
 }
 
 void* platform_allocator(u64 size, b8 alligned){
-
+    return malloc(size);
 }
 void platform_free(void* block, b8 aligned){
-
+    free(block);
 }
 void* platform_zero_memory(void* block, u64 size){
-
+    return memset(block, 0, size);
 }
 void* platform_copy_memory(void* dest, const void* source, u64 size){
-
+    return memcpy(dest, source, size);
 }
 void* platform_set_memory(void* dest, i32 value, u64 size){
-
+    return memset(dest, value, size);
 }
 
 void platform_console_write(const char* message, u8 colour){
+    //FATAL, WARN, ERROR, INFO, DEBUG, TRACE
+    const char* colour_strings[] = {"0;41", "1;31", "1;33", "1;32", "1;34", "1;30"};
 
+    printf("\033[%sm%s\033[0m", colour_strings[colour], message);
 }
 void platform_console_write_error(const char* message, u8 colour){
+    //FATAL, WARN, ERROR, INFO, DEBUG, TRACE
+    const char* colour_strings[] = {"0;41", "1;31", "1;33", "1;32", "1;34", "1;30"};
 
+    printf("\033[%sm%s\033[0m", colour_strings[colour], message);
 }
 
 f64 platform_get_absolute_time(){
+    struct timespec now;
 
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return now.tv_sec + now.tv_nsec * 0.000000001;
 }
 
-//sleep should thread for the provided ms. this blaocks the main thread
-//should only be used for giving time back to the OS for unsued update powere
-//therefor it is not exoported
 void platform_sleep(u64 ms){
-
+    #if _POSTIX_C_SOURCE >= 199309L
+        struct timespec ts;
+        ts.tv_sec = ms / 1000;
+        ts.tv_nsec = (ms % 1000) * 1000* 1000;
+        nanosleep(&ts, 0);
+    #else
+        if(ms >= 1000){
+            sleep(ms / 1000);
+        }
+        usleep((ms % 1000) * 1000);
+    #endif
 }
+
+#endif
